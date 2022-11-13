a403f5cea6d191df8067ac5267a2a84bc472e8df
MatheusReimer <matheusreimer1@gmail.com>
2022-11-13 20:50:29 +0100

    added rows

--- components/header.tsx
+++ components/header.tsx
@@ -22,7 +22,7 @@ function Header() {
     },[])
 
     return (
-        <header className={`${isScrolled && "bg-[#141414]"} `}>
+        <header className={`${isScrolled && "bg-[#141414]"} z-50 `}>
             <div className="flex items-center space-x-2 md:space-x-10">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
                     width={100}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/banner'
 import Header from '../components/header'
+import Row from '../components/row'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
@@ -27,7 +28,6 @@ const Home = ({   netflixOriginals,
   topRated,
   trendingNow,
  }: Props) => {
-  console.log(netflixOriginals)
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h[140vh]">
       <Head>
@@ -35,10 +35,18 @@ const Home = ({   netflixOriginals,
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <main>
+      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals} />
         <section>
-        
+        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
 
